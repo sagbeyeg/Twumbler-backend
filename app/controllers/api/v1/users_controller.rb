5bc5822b68b64ce51b7befec7ea8cee74af6dd70
artwilton <Arthur.M.Wilton@gmail.com>
2020-11-10 14:24:34 -0500

    updated routes for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::UsersController < ApplicationController
+
+  before_action :find_user
+
   def show
-    user = User.find(params[:id])
-    render json: user
+    render json: @user
   end
+
+  # def create
+  #   user = User.create(user_params(:name, age:, bio:))
+  #   render json: user
+  # end
+
+  def update
+	  @user.update(user_params(:name))
+    render json: @user
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params(*args)
+		params.require(:user).permit(*args)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #/api/v1/recipes/1
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
